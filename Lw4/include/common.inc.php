0e7a1ea8cda38b584f7b18a091b06cd303b78ab5
eligantRU <eligant.ru@gmail.com>
2016-05-24 23:17:59 +0400

    Added autoloader(*.inc.php) & removed garbage-file
    Signed-off-by: eligantRU <eligant.ru@gmail.com>

--- Lw4/include/common.inc.php
+++ Lw4/include/common.inc.php
@@ -1,5 +1,11 @@
 <?php
-    require_once('config.inc.php');
-    require_once('string.inc.php');
-    require_once('request.inc.php');
+    require_once("config.inc.php");
 
+    $fileTree = scandir(INCLUDE_DIR);
+    foreach ($fileTree as $fileName)
+    {
+       if (is_file(INCLUDE_DIR . "/" . $fileName) && stristr($fileName, ".inc.php"))
+       {
+           require_once($fileName);
+       }
+    }
--- Lw4/include/config.inc.php
+++ Lw4/include/config.inc.php
@@ -1,7 +1,8 @@
 <?php
     error_reporting(E_ALL);
     define('file', 'D:\\server2go\\htdocs\\!!!K_cdache\\6lab\\data\\');
-    define('SITE_ROOT', 'D:\\server2go\\htdocs\\!!!K_cdache\\6lab');
+    define('SITE_ROOT', 'E:\!git\web_backend\server2go\server2go\htdocs\web_backend\Lw4');
+    define('INCLUDE_DIR', SITE_ROOT . '\include');
     define('DB_HOST', 'localhost');
     define('DB_PORT', '7188');
     define('DB_NAME', 'data');
